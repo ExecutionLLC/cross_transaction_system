202f8c57b9a1d0e43aa3cedd0f5e735c39346361
dmitry <dmitry.zyryanov@execution.su>
2018-08-31 18:23:49 +0700

    services tab: show profile error and serialized data

--- frontend/src/Views/Profile/Profile.jsx
+++ frontend/src/Views/Profile/Profile.jsx
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
 import ViewBase from '../ViewBase';
+import API from '../../API/API';
+import ErrorPanel from '../../Components/ErrorPanel';
 
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      profileError: null,
+      profileData: null,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      isLoading: true,
+      profileError: null,
+      profileData: null,
+    });
+    API.getProfile()
+      .then((profileData) => {
+        this.setState({
+          isLoading: false,
+          profileError: null,
+          profileData,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          isLoading: false,
+          profileError: error,
+          profileData: null,
+        });
+      });
+  }
+
   render() {
+    const { isLoading, profileError, profileData } = this.state;
     return (
-      <ViewBase {...this.props} pageHeader="Profile" />
+      <ViewBase {...this.props} pageHeader="Profile">
+        {isLoading && 'Загрузка...'}
+        {profileData && JSON.stringify(profileData)}
+        {profileError && (
+          <ErrorPanel
+            title="Ошибка получения данных профиля"
+            content={profileError.message}
+          />
+        )}
+      </ViewBase>
     );
   }
 }
