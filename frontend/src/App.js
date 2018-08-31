fe9ad4b71f3822e06ea82fff9c8768bc3d4524cd
dmitry <dmitry.zyryanov@execution.su>
2018-08-31 16:06:07 +0700

    unwanted code removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { PageHeader, Panel } from 'react-bootstrap';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <PageHeader>
           Page header
         </PageHeader>
@@ -16,13 +14,6 @@ class App extends Component {
           </Panel.Heading>
           <Panel.Body>Panel body</Panel.Body>
         </Panel>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
