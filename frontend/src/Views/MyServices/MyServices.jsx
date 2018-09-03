b13b940e6dd1cb3847cebc2c6b7da92544b358c9
dmitry <dmitry.zyryanov@execution.su>
2018-09-03 13:24:17 +0700

    my services tab: load info

--- frontend/src/Views/MyServices/MyServices.jsx
+++ frontend/src/Views/MyServices/MyServices.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import ViewBase from '../ViewBase';
+import API from '../../API/API';
+import ErrorPanel from '../../Components/ErrorPanel';
 
 
 class MyServices extends Component {
@@ -11,13 +13,52 @@ class MyServices extends Component {
       loadingError: null,
       isAddingService: false,
       addindOperatorToService: null,
+      operators: null,
       myServices: null,
     };
   }
 
+  componentDidMount() {
+    this.setState({
+      isLoading: true,
+    });
+    API.getOperators()
+      .then((operators) => {
+        this.setState({
+          operators,
+        });
+        return API.getMyServices()
+          .then((myServices) => {
+            this.setState({
+              isLoading: false,
+              loadingError: false,
+              myServices,
+            });
+          })
+          .catch(() => {
+            this.setState({
+              isLoading: false,
+              loadingError: 'Не загрузился список собственных сервисов',
+            });
+          });
+      })
+      .catch(() => {
+        this.setState({
+          isLoading: false,
+          loadingError: 'Не загрузился список операторов',
+        });
+      });
+  }
+
   render() {
+    const {
+      isLoading, loadingError, operators, myServices,
+    } = this.state;
     return (
-      <ViewBase {...this.props} pageHeader="Мои сервисы">
+      <ViewBase {...this.props} pageHeader="Мои сервисы" isLoading={isLoading}>
+        {loadingError && <ErrorPanel title="Ошибка загрузки" content={loadingError} />}
+        {operators && JSON.stringify(operators)}
+        {myServices && JSON.stringify(myServices)}
         <Button>
           Добавить сервис
         </Button>
