65ac2290dee7005a267e9934f2d8da91aa950c51
dmitry <dmitry.zyryanov@execution.su>
2018-09-06 18:34:13 +0700

    enter wallet view

---
+++ CTSTerminal/src/Components/SmallSpinner.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Spinner } from 'native-base';
+
+class SmallSpinner extends Component {
+  render() {
+    return (
+      <Spinner size="small" color="#000" />
+    );
+  }
+}
+
+
+export default SmallSpinner;
---
+++ CTSTerminal/src/Views/EnterWallet.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import {
+  Container,
+  Header,
+  Left,
+  Form,
+  Item,
+  Input,
+  Content,
+  Body,
+  Title,
+  Button,
+  Icon,
+} from 'native-base';
+import SmallSpinner from '../Components/SmallSpinner';
+
+
+class EnterWallet extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      walletId: '',
+      isLoading: false,
+      error: null,
+    };
+  }
+
+  onWalletChange(walletId) {
+    this.setState({
+      walletId
+    });
+  }
+
+  onSubmit() {
+
+  }
+
+  render() {
+    const {
+      walletId,
+      isLoading,
+      error,
+    } = this.state;
+    return (
+      <Container>
+        <Header>
+          <Body>
+            <Title>Ввод кошелька</Title>
+          </Body>
+        </Header>
+        <Content>
+          <Form>
+            <Item error={error && error.walletError}>
+              <Input
+                placeholder="Кошелёк"
+                autoCorrect={false}
+                autoCapitalize="none"
+                value={walletId}
+                disabled={isLoading}
+                onChangeText={walletId => this.onWalletChange(walletId)}
+              />
+            </Item>
+            {error && (
+              <Text>
+                Error:
+                {error.message}
+              </Text>
+            )}
+            {isLoading ? (
+              <SmallSpinner />
+            ) : (
+              <Button
+                block
+                onPress={() => this.onSubmit()}
+              >
+                <Text>Ввод</Text>
+              </Button>
+            )
+            }
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+
+export default EnterWallet;
