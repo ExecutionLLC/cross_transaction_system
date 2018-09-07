8fb445fbe641a8e3ad9d9a77f83c9eaf194c61d0
andrey.ableev <andre.ableev@execution.su>
2018-09-07 20:03:36 +0700

    добавлен невидимый элемент в контент страницы, чтобы футер не закрывал содержимого страницы

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -23,37 +23,40 @@ const footerStyle = {
 function Footer({ children }) {
   return (
     <div>
-      <div style={footerStyle}>
-        <Grid>
-          <Row>
-            <Col sm={4}>
-              <div style={{ fontSize: '18px' }}>
-                Powered by:
-                <span style={{ paddingLeft: '15px' }}>
-                  <a href="https://www.hyperledger.org/projects/fabric">
-                    <Image src={hyperledger} alt="logo" style={{ maxHeight: '30px' }} />
-                  </a>
-                </span>
-              </div>
-            </Col>
-            <Col sm={4}>
-              <div style={{ fontSize: '18px' }}>
-                Produced by:
-                <span style={{ paddingLeft: '15px' }}>
-                  <a href="https://execution.su/">
-                    <Image src={execution} alt="logo" style={{ maxHeight: '25px' }} />
-                  </a>
-                </span>
-              </div>
-            </Col>
-            <Col sm={4}>
-              <div style={{ fontSize: '16px', paddingTop: '5px' }}>
-                © 2011 - 2018 Execution. All rights reserved.
-              </div>
-            </Col>
-          </Row>
-        </Grid>
-        {children}
+      <div style={{ marginBottom: '50px' }} />
+      <div>
+        <div style={footerStyle}>
+          <Grid>
+            <Row>
+              <Col sm={4}>
+                <div style={{ fontSize: '18px' }}>
+                  Powered by:
+                  <span style={{ paddingLeft: '15px' }}>
+                    <a href="https://www.hyperledger.org/projects/fabric">
+                      <Image src={hyperledger} alt="logo" style={{ maxHeight: '30px' }} />
+                    </a>
+                  </span>
+                </div>
+              </Col>
+              <Col sm={4}>
+                <div style={{ fontSize: '18px' }}>
+                  Produced by:
+                  <span style={{ paddingLeft: '15px' }}>
+                    <a href="https://execution.su/">
+                      <Image src={execution} alt="logo" style={{ maxHeight: '25px' }} />
+                    </a>
+                  </span>
+                </div>
+              </Col>
+              <Col sm={4}>
+                <div style={{ fontSize: '16px', paddingTop: '5px' }}>
+                  © 2011 - 2018 Execution. All rights reserved.
+                </div>
+              </Col>
+            </Row>
+          </Grid>
+          {children}
+        </div>
       </div>
     </div>
   );
