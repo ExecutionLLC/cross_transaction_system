3f9984b87be091d132e5adf05f7cfa24127db9fe
MadHatter <ivkalgin@gmail.com>
2018-09-07 13:34:29 +0700

    now we wait result of transaction request

--- backend/src/models/TransactionModel.js
+++ backend/src/models/TransactionModel.js
@@ -3,7 +3,7 @@ const BaseModel = require('./BaseModel');
 class TransactionModel extends BaseModel {
   add(transaction) {
     const request = this._chaincodeApi.createInvokeRequest('addTransaction', [JSON.stringify(transaction)]);
-    return this._chaincodeApi.sendInvokeRequest(request);
+    return this._chaincodeApi.sendInvokeRequest(request, true);
   }
 }
 
