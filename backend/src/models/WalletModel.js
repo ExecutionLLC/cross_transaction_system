372ac4d30924d12b9d913abf5cdb376d213e0533
MadHatter <ivkalgin@gmail.com>
2018-09-10 18:43:32 +0700

    HOTFIX: changed default limit on transactions

--- backend/src/models/WalletModel.js
+++ backend/src/models/WalletModel.js
@@ -3,7 +3,7 @@ const BaseModel = require('./BaseModel');
 class WalletModel extends BaseModel {
   get(processingName, walletId, offset, limit) {
     const offsetArg = offset ? offset.toString() : '0';
-    const limitArg = limit ? limit.toString() : '10';
+    const limitArg = limit ? limit.toString() : '50';
     const requestArgs = [processingName, walletId, offsetArg, limitArg];
     const request = this._chaincodeApi.createQueryRequest('getWallet', requestArgs);
     return this._chaincodeApi.sendQueryRequest(request);
